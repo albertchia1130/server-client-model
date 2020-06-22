103a7283eafc1d1b81da47a546beeda89cc928d2
root <root@localhost.localdomain>
2020-06-23 05:18:18 +0800

    first commit

---
+++ save.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field]8-
+  $name = $_POST['fname'];
+  $pssword=$_POST['password'];
+  var_dump($name);
+  $servername = "localhost";
+  $username = "root";
+  $password = "albert";
+  $conn =  mysqli_connect($servername,$username,$password);
+	if ($conn){
+		$sql = "USE USER";
+		if (mysqli_query($conn, $sql)) {
+			echo "Database connected successfully";
+		} else {
+			echo "Error creating database: " . mysqli_error($conn);
+		}
+		$sql = "INSERT INTO username1 (name,password) VALUES('$name','$pssword')";
+		if (mysqli_query($conn, $sql)) {
+			echo "INSERT successfully";
+		} else {
+			echo "Error creating data " . mysqli_error($conn);
+		}
+
+
+	}else{
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+
+}
+
+?>
+
